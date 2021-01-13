c370a4f02b5ec93d2a9bfb91c0aa69c7476cdb21
mjbower <martin.bower@maersk.com>
2021-01-13 17:24:20 +0000

    day 9 completed

--- days/day09.go
+++ days/day09.go
@@ -3,16 +3,11 @@ package days
 import (
 	"fmt"
 	inputs "github.com/mjbower/adventOfCode/inputs"
+	"os"
 	"strconv"
 )
 
 // https://adventofcode.com/2020/day/9
-
-pwd
-
-func checkVals(*v []int, i int) {
-
-}
 
 func Nine() {
 	inputSlice = inputs.Day9
@@ -22,9 +17,30 @@ func Nine() {
 	for i, s :=  range inputSlice {
 		v,_ := strconv.Atoi(s)
 		values[i] = v
-		fmt.Printf("Line (%d) Val(%s)\n",i,s)
 	}
-	fmt.Println(values)
 
-	checkVals(&values, 5)
+	ptr := 5
+	for i, _ :=  range values {
+		if i < ptr {
+			continue
+		}
+
+		checkVal := values[ptr]
+		fmt.Printf("Checking for (%d) - ",checkVal)
+		found := false
+		for outer := ptr-5; outer < ptr; outer++ {
+			for inner := outer+1; inner < ptr; inner++ {
+				if values[outer] + values[inner] == checkVal {
+					found = true
+					fmt.Printf("  Found %d + %d = %d\n",values[outer],values[inner],checkVal)
+					break
+				}
+			}
+		}
+		if ! found {
+			fmt.Println("Game over, not found")
+			os.Exit(0)
+		}
+		ptr++
+	}
 }
